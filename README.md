838d00b0c9138546f8324221dfa1ec88ca96b90d
Muhammed Ubuntu server <mtangirkul@gmail.com>
2024-08-30 19:30:26 +0000

    Start on Gunicorn

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # addALanguageForMe
 gpt-3.5-turbo
+
+For start server:
+gunicorn -w 4 -b 0.0.0.0:8000 server:app
--- server.py
+++ server.py
@@ -22,6 +22,3 @@ def test():
   return ("Hello")
 
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=80)
-
